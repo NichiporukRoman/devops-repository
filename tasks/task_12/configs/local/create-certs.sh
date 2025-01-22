a310569cecd85442dd18bfe5e68303b6d3d1afa4
NichiporukRoman <romannichiporukk@gmail.com>
2025-01-22 23:10:27 +0300

    huh

--- tasks/task_12/configs/local/create-certs.sh
+++ tasks/task_12/configs/local/create-certs.sh
@@ -1,5 +1,5 @@
-mkdir -p /etc/ssl/self-signed
-cd /etc/ssl/self-signed 
+mkdir -p /home/ubuntu1/self-signed
+cd /home/ubuntu1/self-signed 
 openssl genrsa -out selfsigned.key 2048 
 openssl req -x509 -new -nodes -key selfsigned.key -sha256 -days 365 \
     -out selfsigned.crt \

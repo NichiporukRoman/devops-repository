4ef5338d48a69a5ef8bede2b597eca8722a99ed2
NichiporukRoman <romannichiporukk@gmail.com>
2025-01-22 23:14:39 +0300

    huh

--- tasks/task_12/configs/local/create-certs.sh
+++ tasks/task_12/configs/local/create-certs.sh
@@ -1,6 +1,9 @@
 mkdir -p /home/ubuntu1/self-signed
-cd /home/ubuntu1/self-signed 
+cd /home/ubuntu1/self-signed
 openssl genrsa -out selfsigned.key 2048 
 openssl req -x509 -new -nodes -key selfsigned.key -sha256 -days 365 \
     -out selfsigned.crt \
     -subj "/C=US/ST=State/L=City/O=Organization/OU=Department/CN=n8ire.zapto.org/"
+chmod 777 /home/ubuntu1/self-signed/
+chmod 777 /home/ubuntu1/self-signed/selfsigned.key
+chmod 777 /home/ubuntu1/self-signed/selfsigned.crt 
